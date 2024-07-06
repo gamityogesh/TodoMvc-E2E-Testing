eaf3fa42a1f43907c45563d0e0586921fcc5815b
Yogesh Gamit <yogeshgamit4833@gmail.com>
2024-07-06 08:50:44 +0530

    add todo

--- tests/todo.spec.js
+++ tests/todo.spec.js
@@ -1,6 +1,16 @@
 const {test,expect} =require('@playwright/test')
-test("ext field is cleared when item is added", async ({page})=>{
+test("Text field is cleared when item is added", async ({page})=>{
     await page.goto("https://demo.playwright.dev/todomvc/#/")
-    await page.fill(".new-todo","water the plant")
-   
+    await page.locator(".new-todo").fill("water the plant")
+    await page.locator(".new-todo").press('Enter')
+    await expect(page.locator(".new-todo")).toBeEmpty()
+    await page.locator(".toggle").first().click()
+    await expect(page.locator(".toggle")).toBeChecked()
+    await page.locator("[href*='completed']").click()
+    await expect(page.locator(".toggle")).toBeChecked()
+    await page.locator(".clear-completed").click()
+    await expect(page.locator(".new-todo")).toBeEmpty()
+
+
+
 }) 
